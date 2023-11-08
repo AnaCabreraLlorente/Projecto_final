998d59760f730d0ed49420a97908deb231728dd9
AnaCabreraLlorente <ana.cabrera.llorente@ieselcalamot.com>
2023-11-08 18:45:25 +0100

    fisrt commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        getSupportActionBar().hide();
+        ImageButton botonAtras = findViewById(R.id.botonAtras);
+        botonAtras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }}
+
---
+++ app/src/main/java/com/example/myapplication/Response.java
@@ -0,0 +1,22 @@
+//package com.example.myapplication;
+//
+//import com.google.gson.annotations.SerializedName;
+//
+//public class Response {
+//    @SerializedName("status")
+//    private String status;
+//
+//    @SerializedName("result_code")
+//    private int result_code;
+//
+//    // getter
+//
+//
+//    public String getStatus() {
+//        return status;
+//    }
+//
+//    public int getResult_code() {
+//        return result_code;
+//    }
+//}
